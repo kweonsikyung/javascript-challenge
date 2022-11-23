3e077ebe27e33aab51171fa405bba0db00abd14e
kweonsikyung <ekrk6632@naver.com>
2022-11-24 04:09:21 +0900

    add : notes-app

---
+++ notes-app/script.js
@@ -0,0 +1,77 @@
+const addBtn = document.getElementById('add');
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    });
+}
+
+addBtn.addEventListener('click', ()=>{
+    addNewNote();
+});
+
+function addNewNote(text=''){
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML= `
+        <div class="notes">
+        <div class="tools">
+            <button class="edit">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button class="delete">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+        </div>
+        <!-- textarea가 default인 상태 -->
+        <div class="main ${text ? '' : 'hidden'}"> </div>
+        <textarea class = "${text ? 'hidden' : ''}"></textarea>   
+        </div>
+    </div>`;
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked.parse(text);
+
+    editBtn.addEventListener('click', () =>{
+        textArea.classList.toggle('hidden');
+        main.classList.toggle('hidden');
+    });
+
+    deleteBtn.addEventListener("click", () =>{
+        note.remove();
+
+        updateLS();
+    });
+
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+
+        main.innerHTML = marked.parse(value);
+
+        updateLS();
+        
+    });
+
+    document.body.appendChild(note);
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+};
