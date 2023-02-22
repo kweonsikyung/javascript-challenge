7d3ac70c4fa3156e36ea10446b953324bf7c8dda
kweonsikyung <ekrk6632@naver.com>
2023-02-22 09:05:27 +0900

    백업

---
+++ passworld generator/script.js
@@ -0,0 +1,98 @@
+const pw = document.getElementsById('pw');
+const copy = document.getElementsById('copy');
+const len = document.getElementsById('len');
+const upper = document.getElementsById('upper');
+const lower = document.getElementsById('lower');
+const number = document.getElementsById('number');
+const symbol = document.getElementsById('symbol');
+const generate = document.getElementsById('generate');
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
+
+function getLowerCase(){
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+function getUpperCase(){
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+function getNumber(){
+    return numbers[Math.floor(Math.random() * upperLetters.length)];
+}
+function getSymbol(){
+    return symbols[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function generatePassword() {
+    const len = lenEl.value;
+
+    let password = "";
+
+    if (upperEl.checked) {
+        password += getUppercase();
+    }
+
+    if (lowerEl.checked) {
+        password += getLowercase();
+    }
+
+    if (numberEl.checked) {
+        password += getNumber();
+    }
+
+    if (symbolEl.checked) {
+        password += getSymbol();
+    }
+
+    for (let i = password.length; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+function generateX() {
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+    if (xs.length === 0) return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
+
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = pwEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+});
